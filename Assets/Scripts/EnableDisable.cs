1d7bc63c1727a54ce7d00f53ceefc06f2a8c1b6a
Sam Vance <vancesa@shernet.sheridancollege.ca>
2025-01-30 14:40:49 -0500

    Made some noise

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -7,6 +7,8 @@ public class EnableDisable : MonoBehaviour
     public SpriteRenderer sprite;
     public EnableDisable script;
     public GameObject go;
+    public AudioSource audioSource;
+    public AudioClip clip;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +34,13 @@ public class EnableDisable : MonoBehaviour
             go.SetActive(true);
         }
 
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (audioSource.isPlaying == false)
+            {
+                // audioSource.Play();
+                audioSource.PlayOneShot(clip);
+            }
+        }
     }
 }
